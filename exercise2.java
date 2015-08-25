b7ee1a10757bf6f5436fac6061931f1e5db0db72
ievutec <ievutec@gmail.com>
2015-08-25 18:16:59 +0100

    Update exercise2.java

--- exercise2.java
+++ exercise2.java
@@ -1,1 +1,12 @@
 
+
+Integer hasMajority(ArrayList<Integer> list){
+  list.sort();
+  int lim = list.size()/2;
+  while(!list.isEmpty()){
+    int number = lastIndexOf(list.get(0)) + 1;
+    if(number > lim) return list.get(0);
+    list.subList(0, number).clear();
+  }
+  return null;
+}
