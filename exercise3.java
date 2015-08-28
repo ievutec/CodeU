2085031d3084849bb6639015742fdf5a1da7597a
ievutec <ievutec@gmail.com>
2015-08-28 19:10:51 +0100

    Update exercise3.java

--- exercise3.java
+++ exercise3.java
@@ -1,5 +1,36 @@
 
 public class Largest{
   
-  public Largest(Array)
+  private Integer[] array;
+  private int sorted;
+  
+  public Largest(Integer[] array) throws IllegalArgumentException{
+    if(array.length == 0) throw new IllegalArgumentException("The array is empty.");
+    this.array = array;
+    sorted = 0;
+    sort();
+  }
+  
+  public Integer nthLargest(int n) throws IllegalArgumentException{
+    if(n < 1 || n >= array.length) throw new IllegalArgumentException("The input is out of bounds.");
+    if(n < sorted) return array[n-1];
+    else{
+      for(int i = sorted; i < n; i++) this.sort();
+      return array[n-1];
+    }
+    
+  }
+  
+  private void sort(){
+    int max = array[sorted+1];
+    int temp = 0;
+    for (int i = sorted; i < array.length; i++){
+      if(array[i] >= max) {
+        temp = i
+      }
+    }
+    int num = array[sorted + 1];
+    array[sorted + 1] = array[temp];
+    array[temp] = num;
+  }
 }
