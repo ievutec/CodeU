22c0c7a91d4e75639a518802194d451ea5832247
ievutec <ievutec@gmail.com>
2015-09-03 15:37:24 +0100

    Create Session4.java

---
+++ Session4.java
@@ -0,0 +1,11 @@
+
+// Exercise 1
+
+// I decided to simplify the task by not using trailing zeros, although
+// if I have time I'll modify the original to configure to trailing zeros.
+
+// If there is a way to make the function faster, I think I would create an
+// array to keep track of the integers I already know are pallindromes and configure
+// the size of the array to whatever is appropriate for its usage.
+
+public int bitPallindrome(int K){
